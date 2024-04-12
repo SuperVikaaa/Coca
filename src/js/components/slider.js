93e4262f3e646ca7cbf4e8b8d9222412fb9376aa
SuperVikaaa <supervikaaa@gmail.com>
2024-04-12 12:11:43 +0300

    Created testimonials section

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,4 +1,5 @@
 import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
 import 'swiper/css';
 
 export const useInsightSlider = () => {
@@ -12,6 +13,19 @@ export const useInsightSlider = () => {
       993: {
         centeredSlides: false,
       },
+    },
+  });
+};
+
+export const useTestimonialsSlider = () => {
+  new Swiper('.testimonials__slider', {
+    modules: [Navigation],
+    slidesPerView: 'auto',
+    spaceBetween: 22,
+    loop: true,
+    navigation: {
+      prevEl: '.testimonials__btn--prev',
+      nextEl: '.testimonials__btn--next',
     },
   });
 };
