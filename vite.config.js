423b54d0f5cfb6edebb08841499cf8141ca5f211
SuperVikaaa <supervikaaa@gmail.com>
2024-05-08 14:49:34 +0300

    Fixed burger and links

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
   root: './src',
-  base: '/Coca/',
+  base: '/coca/',
   server: {
     port: 3000,
     open: true,
