4085e76473a49c613e86d2f534effac826d43d63
SuperVikaaa <supervikaaa@gmail.com>
2024-05-13 12:02:18 +0300

    Fixed tooltip

--- src/js/components/contact/map.js
+++ src/js/components/contact/map.js
@@ -2,11 +2,11 @@ export const useMap = () => {
 const tooltip = document.querySelector('.hero__tooltip');
 const continent = document.querySelector('.hero__pin');
 
-continent.addEventListener('mousemove', function (e) {
-  tooltip.innerText = this.dataset.title;
-  tooltip.style.top = e.y + 'px';
-  tooltip.style.left = e.x + 'px';
-});
+// continent.addEventListener('mousemove', function (e) {
+//   tooltip.innerText = this.dataset.title;
+//   tooltip.style.top = (e.y + 20) + 'px';
+//   tooltip.style.left = (e.x + 20) + 'px';
+// });
 
 continent.addEventListener('mouseenter', function () {
   tooltip.style.display = 'block';
